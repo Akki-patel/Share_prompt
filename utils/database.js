7cc2e5f220183cdfbb0a706ed42eb94075f1acf8
Akshay <akshaysangani44@gmail.com>
2024-04-20 19:25:33 +0530

    update

--- utils/database.js
+++ utils/database.js
@@ -5,20 +5,20 @@ let isConnected = false; // track the connection
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-  if(isConnected) {
+  if (isConnected) {
     console.log('MongoDB is already connected');
     return;
   }
 
   try {
-    await mongoose.connect(String(process.env.MONGODB_URI), {
+    const uri = process.env.MONGODB_URI.toString(); // Convert URI to string
+    await mongoose.connect(uri, {
       dbName: "share_prompt",
-   
-    })
+    });
 
     isConnected = true;
 
-    console.log('MongoDB connected')
+    console.log('MongoDB connected');
   } catch (error) {
     console.log(error);
   }
