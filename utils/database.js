a14f28b7f58609ff06bd1bdc4377a7d4614ed222
Akshay <akshaysangani44@gmail.com>
2024-04-21 11:35:30 +0530

    to string removed

--- utils/database.js
+++ utils/database.js
@@ -5,20 +5,20 @@ let isConnected = false; // track the connection
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-  if (isConnected) {
+  if(isConnected) {
     console.log('MongoDB is already connected');
     return;
   }
 
   try {
-    const uri = process.env.MONGODB_URI.toString(); // Convert URI to string
-    await mongoose.connect(uri, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
-    });
+   
+    })
 
     isConnected = true;
 
-    console.log('MongoDB connected');
+    console.log('MongoDB connected')
   } catch (error) {
     console.log(error);
   }
